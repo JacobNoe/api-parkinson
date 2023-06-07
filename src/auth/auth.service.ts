e6d62d7e1020d80aa8402bf8d3ee3914837cac54
JacobNoe <jacobnoegarciadimas@gmail.com>
2023-06-06 23:01:47 -0600

    payload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,6 +22,7 @@ export class AuthService {
 
   async login(user: any) {
     const payload = { correo: user.Correo, sub: user.id };
+    console.log("PAYLOAD", payload);
     return {
       access_token: this.jwtService.sign(payload),
     };
